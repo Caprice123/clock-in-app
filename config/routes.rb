a5904bbef948f8edebc4be6edbe7687d198a3fe2
Caprice123 <kaguya260401@gmail.com>
2025-09-07 21:05:55 +0700

    add missing route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :follows, only: %i[create destroy]
+      resources :sleep_records, only: %i[create]
     end
   end
 end
